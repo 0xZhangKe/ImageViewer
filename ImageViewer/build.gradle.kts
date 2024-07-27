f0e1eaf5e77538680e92dcf7122325832378f967
ZhangKe <kezhang404@gmail.com>
2024-07-27 22:18:10 +0800

    upgrade version to 0.1.3

--- ImageViewer/build.gradle.kts
+++ ImageViewer/build.gradle.kts
@@ -55,7 +55,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.zhangke.imageviewer"
                 artifactId = "image-viewer"
-                version = "0.1.1"
+                version = "0.1.3"
 
                 from(components["release"])
             }
